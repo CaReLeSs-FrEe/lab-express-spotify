3f2c44633354e27b59ee1aaa3ea82ea84d5ed482
Alejandro Mejia <mejia.alejandro1@outlook.com>
2022-09-12 00:45:13 -0400

    first commit

--- app.js
+++ app.js
@@ -2,8 +2,7 @@ require('dotenv').config();
 
 const express = require('express');
 const hbs = require('hbs');
-
-// require spotify-web-api-node package here:
+const SpotifyWebApi = require('spotify-web-api-node');
 
 const app = express();
 
@@ -11,8 +10,34 @@ app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 
-// setting the spotify-api goes here:
+const spotifyApi = new SpotifyWebApi({
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET
+  });
 
-// Our routes go here:
+spotifyApi
+  .clientCredentialsGrant()
+  .then(data => spotifyApi.setAccessToken(data.body['access_token']))
+  .catch(error => console.log('Something went wrong when retrieving an access token', error));
+
+spotifyApi
+  .searchArtists(Artist)
+  .then(data => {
+    console.log('The received data from the API: ', data.body);
+    // ----> 'HERE WHAT WE WANT TO DO AFTER RECEIVING THE DATA FROM THE API'
+  })
+  .catch(err => console.log('The error while searching artists occurred: ', err));
+
+app.get('/', (req, res) => {
+    res.render('Home.hbs');
+});
+
+app.get('/artist-search', (req, res) => {
+    const artist = 'Lupe Fiasco'
+    // 1) first, you have to get the artist information (question remains: how do i get the artist name the user sends me through my home.hbs form?)
+
+    // 2) Finally, render the page with the artist information returned by step one
+    res.render('Home.hbs');
+});
 
 app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
