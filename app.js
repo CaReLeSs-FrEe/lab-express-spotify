7a5f2714c4e5afe39496dd9b038eb53194b40d21
Alejandro Mejia <mejia.alejandro1@outlook.com>
2022-09-15 01:09:20 -0400

    1 thru 4 done

--- app.js
+++ app.js
@@ -15,29 +15,60 @@ const spotifyApi = new SpotifyWebApi({
     clientSecret: process.env.CLIENT_SECRET
   });
 
+
 spotifyApi
   .clientCredentialsGrant()
   .then(data => spotifyApi.setAccessToken(data.body['access_token']))
   .catch(error => console.log('Something went wrong when retrieving an access token', error));
 
-spotifyApi
-  .searchArtists(Artist)
-  .then(data => {
-    console.log('The received data from the API: ', data.body);
-    // ----> 'HERE WHAT WE WANT TO DO AFTER RECEIVING THE DATA FROM THE API'
-  })
-  .catch(err => console.log('The error while searching artists occurred: ', err));
+
 
 app.get('/', (req, res) => {
     res.render('Home.hbs');
 });
 
-app.get('/artist-search', (req, res) => {
-    const artist = 'Lupe Fiasco'
-    // 1) first, you have to get the artist information (question remains: how do i get the artist name the user sends me through my home.hbs form?)
+app.get('/artist-search', async (req, res) => {
+    try {
+      const artist = req.query.artist || 'Lupe Fiasco'
+      const data = await spotifyApi.searchArtists(artist)
+      // console.log('The received data from the API: ', JSON.stringify(data.body, null, 2));
+      const artistInformationCleanedUp = data.body.artists?.items?.map(oneArtist => {
+        const randomImageIndex = Math.floor(Math.random() * oneArtist.images.length)
+        return {
+          name: oneArtist.name,
+          image: oneArtist.images[randomImageIndex]?.url,
+          artistId: oneArtist.id
+        }
+      })
+      const info = {
+        artist: artistInformationCleanedUp
+      }
+      // res.send(data.body) // to better visualize the data structure of the artist information
+      res.render('artist-search-results.hbs', info);
+    } catch (error) {
+      console.log('The error while searching artists occurred: ', error);
+    } 
+});
 
-    // 2) Finally, render the page with the artist information returned by step one
-    res.render('Home.hbs');
+app.get('/albums/:artistId', async (req, res, next) => {
+  try {
+    const artistId = req.params.artistId || '74D1UgRzMhTSPz698exXmR'
+    console.log({ artistId: artistId })
+    const data = await spotifyApi.getArtistAlbums(artistId)
+    // console.log('The received data from the API: ', JSON.stringify(data.body, null, 2));
+    const artistAlbums = data.body.items.map(oneAlbum => {
+      console.log('One Album', oneAlbum)
+      const randomImageIndex = Math.floor(Math.random() * oneAlbum.images.length)
+      return {
+        name: oneAlbum.name,
+        cover: oneAlbum.images[randomImageIndex]?.url,
+        id: oneAlbum.id
+      }
+    })
+    res.render('albums.hbs', artistAlbums);
+  } catch (error) {
+    console.log('The error while searching albums occurred: ', error);
+  } 
 });
 
 app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
